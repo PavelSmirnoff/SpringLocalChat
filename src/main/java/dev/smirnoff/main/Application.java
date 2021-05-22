fe2fca7937c798a5c400acc192668c031120078d
Pavel Smirnov <smirnov@0x55.ru>
2021-05-22 19:41:34 +0300

    Use Git

---
+++ src/main/java/dev/smirnoff/main/Application.java
@@ -0,0 +1,20 @@
+package dev.smirnoff.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring local Chat for personal use
+ *
+ * @author pavelsmirnov
+ * @version 0.1
+ * Created 22.05.2021
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
